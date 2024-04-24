f58c4384867f102ddc098c85f0e0e69b6d168ec5
Ahmed Khaled <ahmedkhaled4d@gmail.com>
2024-04-24 16:03:11 +0200

    .env

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,7 @@
+import { env } from "./utils/env";
+
 async function main() {
-  console.log("Typescript gemini-pro Lib");
+  console.log("Typescript gemini-pro Lib : API KEY");
+  console.log(env.API_KEY);
 }
 main();
---
+++ src/utils/common.ts
@@ -0,0 +1,53 @@
+import {
+  ChatSession,
+  CountTokensRequest,
+  GenerativeModel,
+  GoogleGenerativeAI,
+} from "@google/generative-ai";
+import fs from "fs";
+import { Readable } from "stream";
+
+// Get your API key from https://makersuite.google.com/app/apikey
+// Access your API key as an environment variable
+export const genAI = new GoogleGenerativeAI(process.env.API_KEY!);
+
+// Converts local file information to a GoogleGenerativeAI.Part object
+export function fileToGenerativePart(path: string, mimeType: string) {
+  return {
+    inlineData: {
+      data: Buffer.from(fs.readFileSync(path)).toString("base64"),
+      mimeType,
+    },
+  };
+}
+
+// Prints chunks of generated text to the console as they become available
+export async function streamToStdout(stream: Readable) {
+  console.log("Streaming...\n");
+  for await (const chunk of stream) {
+    // Get first candidate's current text chunk
+    const chunkText = chunk.text();
+    // Print to console without adding line breaks
+    process.stdout.write(chunkText);
+  }
+  // Print blank line
+  console.log("\n");
+}
+
+export async function displayTokenCount(
+  model: GenerativeModel,
+  request: CountTokensRequest
+) {
+  const { totalTokens } = await model.countTokens(request);
+  console.log("Token count: ", totalTokens);
+}
+
+export async function displayChatTokenCount(
+  model: GenerativeModel,
+  chat: ChatSession,
+  msg: string
+) {
+  const history = await chat.getHistory();
+  const msgContent = { role: "user", parts: [{ text: msg }] };
+  await displayTokenCount(model, { contents: [...history, msgContent] });
+}
